227646383d294cdfba0a4bd3a9069a4420063817
韦义烨 <523306566@qq.com>
2022-07-07 22:35:52 +0800

    es

--- mall-search/src/main/java/com/market/mall/search/MallSearchApplication.java
+++ mall-search/src/main/java/com/market/mall/search/MallSearchApplication.java
@@ -1,19 +1,17 @@
-package com.market.mall.gateways;
+package com.market.mall.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import rx.internal.util.atomic.LinkedQueueNode;
 
-import java.util.HashMap;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class MallGatewaysApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MallSearchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallGatewaysApplication.class, args);
+        SpringApplication.run(MallSearchApplication.class, args);
     }
 
 }
