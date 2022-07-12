dc0bd42c49104bdfdeff2ba8fcb3cc4d3ed28d1d
韦义烨 <523306566@qq.com>
2022-07-12 23:13:21 +0800

    1

--- mall-search/src/test/java/com/market/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/market/mall/search/MallSearchApplicationTests.java
@@ -5,8 +5,21 @@ import com.market.mall.search.config.mallElasticSearchConfig;
 import lombok.Data;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.avg.Avg;
+import org.elasticsearch.search.aggregations.metrics.avg.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +33,68 @@ import java.io.IOException;
 public class MallSearchApplicationTests {
     @Autowired
     private RestHighLevelClient client;
+
+
+
+    /**
+     * 复杂检索:在bank中搜索address中包含mill的所有人的年龄分布以及平均年龄，平均薪资
+     *
+     * @throws IOException
+     */
+    @Test
+    public void searchData() throws IOException {
+        //1. 创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+
+        //1.1）指定索引
+        searchRequest.indices("bank");
+        //1.2）构造检索条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery("address", "Mill"));
+
+
+        //1.2.1)按照年龄分布进行聚合
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        sourceBuilder.aggregation(ageAgg);
+
+        //1.2.2)计算平均年龄
+        AvgAggregationBuilder ageAvg = AggregationBuilders.avg("ageAvg").field("age");
+        sourceBuilder.aggregation(ageAvg);
+        //1.2.3)计算平均薪资
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        sourceBuilder.aggregation(balanceAvg);
+
+        System.out.println("检索条件：" + sourceBuilder);
+        searchRequest.source(sourceBuilder);
+        //2. 执行检索
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        System.out.println("检索结果：" + searchResponse);
+
+        //3. 将检索结果封装为Bean
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit searchHit : searchHits) {
+            String sourceAsString = searchHit.getSourceAsString();
+            User users = JSON.parseObject(sourceAsString, User.class);
+            System.out.println(users);
+
+        }
+
+        //4. 获取聚合信息
+        Aggregations aggregations = searchResponse.getAggregations();
+
+        Terms ageAgg1 = aggregations.get("ageAgg");
+
+        for (Terms.Bucket bucket : ageAgg1.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("年龄：" + keyAsString + " ==> " + bucket.getDocCount());
+        }
+        Avg ageAvg1 = aggregations.get("ageAvg");
+        System.out.println("平均年龄：" + ageAvg1.getValue());
+
+        Avg balanceAvg1 = aggregations.get("balanceAvg");
+        System.out.println("平均薪资：" + balanceAvg1.getValue());
+    }
 
     @Test
     public void indexData() throws IOException {
